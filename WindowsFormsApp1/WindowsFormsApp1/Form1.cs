c14765ebab4c1464f39c7d16d0091def37a94140
toannguyen <toannguyen.programmer@gmail.com>
2022-02-26 14:25:38 +0700

    commit login form

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -16,5 +16,22 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtUserName.Text == "admin" && txtPassword.Text == "admin") {
+                MessageBox.Show("successful");
+            }
+        }
     }
 }
